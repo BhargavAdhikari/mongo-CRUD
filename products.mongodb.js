09c4144e504b41584a196d767c6df99481d2b8e5
Bhargav Adhikari <bhargavadhikari@Bhargavs-MacBook-Pro.local>
2024-01-08 21:12:26 +0545

    First mongo crud operation

---
+++ products.mongodb.js
@@ -0,0 +1,43 @@
+use("company");
+
+// db.product.insertMany([
+//   { name: "iphone 14ProMax", price: 1100, quantity: 15 },
+//   { name: "Oppo A3", price: 500, quantity: 25 },
+//   { name: "MI Redmi Prime", price: 700, quantity: 50 },
+// ]);
+
+// db.product.updateOne(
+//   { name: "iphone 14ProMax" },
+//   {
+//     $set: {
+//       name: "Iphone 15",
+//       price: 1999,
+//     },
+//   }
+// );
+
+// db.product.updateOne(
+//   { price: 500 },
+//   {
+//     $set: {
+//       price: 1999,
+//     },
+//   }
+// );
+
+// db.product.updateMany(
+//   { price: 1999 },
+//   {
+//     $set: {
+//       quantity: 40,
+//     },
+//   }
+// );
+
+// db.product.deleteOne({ price: 700 });
+
+// db.product.deleteMany({});
+
+// db.product.findOne({ price: 1100 });
+
+// db.product.find();
